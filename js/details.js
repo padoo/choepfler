94d7a7c9919dc63ba73cbc27fe08d18d305656d7
levi.schnyder(u120760) <levi.schnyder@mobi.ch>
2021-09-21 09:37:29 +0200

    Updates

--- js/details.js
+++ js/details.js
@@ -1,37 +1,55 @@
 let json;
-let badname = '';
-let badOrt = '';
 
 let baseUrl = (window.location).search;
 let id = baseUrl.substring(baseUrl.lastIndexOf('=') + 1);
 
-fetch('https://www.wiewarm.ch:443/api/v1/bad.json/'+id)
+fetch('https://www.wiewarm.ch:443/api/v1/bad.json/' + id)
   .then(res => res.json())
   .then(data => json = data)
   .then(appendBeckenlisteToHtmlTable)
 
+function appendBeckenlisteToHtmlTable() {
+  let badname = json['badname'];
+  let badOrt = json['ort'];
+  let badPreis = json['preise'].replaceAll(/(\n)+/g, '<br />');
+  let badAdresse = json['adresse2'];
+  let badPLZ = json['plz'];
 
-function appendBeckenlisteToHtmlTable(){
-  badname = json['badname'];
-  badOrt = json['ort'];
   $("#badname").html(badname + ' ' + badOrt);
+
+  let preis = '';
+  if (badPreis.length > 0) {
+    preis += badPreis;
+  } else
+    preis = 'Keine Angaben vorhanden.'
+
+  $("#badpreise").html(preis);
+
+  let adresse = `<p>${badname} <br>`;
+  if (badAdresse.length > 0) {
+    adresse += `${badAdresse} <br>`;
+  }
+  adresse += `${badPLZ} ${badOrt}</p>`
+
+  $('#badAdresse').append(adresse);
 
   const beckenliste = [];
   const becken = json['becken'];
   for (const [key, value] of Object.entries(becken)) {
     const beckendetails = {
       beckenname: value.beckenname,
-      temperatur: value.temp
+      temperatur: value.temp,
+      dateLastUpdate: new Date(value.date)
     };
     beckenliste.push(beckendetails);
   }
-
 
   beckenliste.forEach(function (becken) {
     $('#beckenListe').append(`
     <tr>
       <th scope="row">${becken.beckenname}</th>
-      <td>${becken.temperatur} °C</td>
+      <td>${becken.temperatur} °C </td>
+      <td>${becken.dateLastUpdate.toLocaleString()}</td>
     </tr>
     `)
   });
--- js/search.js
+++ js/search.js
@@ -26,7 +26,7 @@ async function getBadiPicture(badId) {
   const response = await fetch('https://www.wiewarm.ch:443/api/v1/image.json/' + badId);
   const data = await response.json();
   let bildUrl = "https://www.bern.com/assets/images/1/marzili_03-7ece01a1.jpg";
-  if (data.length > 0){
+  if (data.length > 0) {
     let imageUrl = data[0].image;
     bildUrl = "https://www.wiewarm.ch/" + imageUrl;
   }
@@ -39,23 +39,29 @@ function update_list(suchergebnisse) {
   $('#badiliste').empty();
   suchergebnisse.forEach(function (suchergebnis) {
     $('#badiliste').append(`
-      <a href="details?id=${suchergebnis.id}" class="hiddenlink">
-        <div class="card mb-3">
-          <div class="card-body">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="d-flex proposal">
-                  <img src="${suchergebnis.bildUrl}" class="proposalimage">
-                  <div>
-                    <h5>${suchergebnis.name}</h5>
-                    <p>${suchergebnis.ort}</p>
-                  </div>
-                </div>
+<a href="details?id=${suchergebnis.id}" class="hiddenlink">
+  <div class="card mb-3">
+    <div class="card-body">
+      <div class="col-md-6">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 py-2">
+              <div class="d-flex">
+                <img src="${suchergebnis.bildUrl}" class="img-fluid">
+              </div>
+            </div>
+            <div class="align-middle col-sm-12 col-md-6 col-lg-6 py-2">
+              <div class="text-center">
+                <h5>${suchergebnis.name}</h5>
+                <p>${suchergebnis.ort}</p>
               </div>
             </div>
           </div>
         </div>
-      </a>
+      </div>
+    </div>
+  </div>
+</a>
     `)
   });
 }
